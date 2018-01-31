98e1f78e3a632e2a5953a4d9f3e59c9a611e5f0b
MOHANRAJ971997 <35914794+MOHANRAJ971997@users.noreply.github.com>
2018-01-31 15:11:39 +0530

    Create mohan22.c

---
+++ mohan22.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main()
+{
+   int no, sums = 0, c, array[100];
+ 
+   scanf("%d", &no);
+ 
+   for (c = 0; c < no; c++)
+   {
+      scanf("%d", &array[c]);
+      sums = sums + array[c];
+   }
+ 
+   printf("Sum = %d\n",sums);
+ 
+   return 0;
+}
